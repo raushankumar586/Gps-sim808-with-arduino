9b35ba55b86cd39b99ab5fc83f2f69d5592d3c23
raushankumar586 <raushankumar586@gmail.com>
2019-08-23 08:21:30 +0530

    test ino with init and gps attach success

--- gps/gps.ino
+++ gps/gps.ino
@@ -8,11 +8,29 @@ void setup()
   Serial1.begin(9600);
   delay(1000);
   Serial.println("Setup Complete!");
+  while (!gps.Init()){
+    delay(1000);
+    Serial.println("sim com device pair failed...");
+  }
+  Serial.println("init success!");
+  if(gps.Attach())
+  Serial.println("gps attached Complete!");
+  else
+  Serial.println("gps attached failed");
+
+
 }
 
 void loop()
 {
-  
+  if(gps.GetData()){
+    Serial.println("got data");
+
+  }
+  else {
+    Serial.println("not  data");
+
+  }
 
   
 }
