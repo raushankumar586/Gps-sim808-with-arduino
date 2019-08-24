98c97306a2da1d0c41c3a1bfc561af70b917f476
raushankumar586 <raushankumar586@gmail.com>
2019-08-24 15:16:35 +0530

    added gps id token string

--- gps/handle_gps.h
+++ gps/handle_gps.h
@@ -1,6 +1,7 @@
 #ifndef __HANDLE_GPS__
 #define __HANDLE_GPS__
 #include <Arduino.h>
+
 #define TIMEOUT 4000 // 4 sec
 #define ZERO 0
 #define MAX_GPS_BUFFER_SIZE 100
@@ -21,8 +22,11 @@ public:
 	bool LoadData();
 	bool IsValidData(char *data);
 	bool parseGprmcData(char *data);
+	bool parseGpgllData(char *data);
 	byte bufferIndex=0;
 	byte bufferGps[MAX_GPS_BUFFER_SIZE];
+    // const char *GpggaToken = "$GPGGA";
+    const char *GpggaToken = "$GPGLL";
     
     struct gspdata{
 		float lat;
