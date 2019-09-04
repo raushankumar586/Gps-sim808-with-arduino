db267c8b6a416e959890698b7b216b56168cfeec
raushankumar586 <raushankumar586@gmail.com>
2019-09-04 18:28:28 +0530

    registration related documentation

--- gps/handleSim.cpp
+++ gps/handleSim.cpp
@@ -140,6 +140,40 @@ bool handleSim::connectToNetwork(){
 
     
 }
+/**
+ * @brief 
+ * AT command gives information about the registration status and access technology of the serving cell.
+ * systex : +CREG: accessTechnology,registration_status
+ * 
+ * Possible values of accessTechnology are,
+ * |0|  >>   GSM
+ * |1|  >>   GSM Compact
+ * |2|  >>   UTRAN
+ * |3|  >>   GSM w/EGPRS
+ * |4|  >>   UTRAN w/HSDPA
+ * |5|  >>   UTRAN w/HSUPA
+ * |6|  >>   UTRAN w/HSDPA and HSUPA
+ * |7|  >>   E-UTRAN
+ * 
+ * Possible values of registration status are,
+ * |0|  >>   not registered, MT is not currently searching a new operator to register to
+ * |1|  >>   registered, home network
+ * |2|  >>   not registered, but MT is currently searching a new operator to register to
+ * |3|  >>   registration denied
+ * |4|  >>   unknown (e.g. out of GERAN/UTRAN/E-UTRAN coverage)
+ * |5|  >>   registered, roaming
+ * |6|  >>   registered for "SMS only", home network (applicable only when indicates E-UTRAN)
+ * |7|  >>   registered for "SMS only", roaming (applicable only when indicates E-UTRAN)
+ * |8|  >>   attached for emergency bearer services only (see NOTE 2) (not applicable)
+ * |9|  >>   registered for "CSFB not preferred", home network (applicable only when indicates E-UTRAN)
+ * |10| >>   registered for "CSFB not preferred", roaming (applicable only when indicates E-UTRAN)
+ * 
+ * 
+ * 
+ * 
+ * @return true 
+ * @return false 
+ */
 
 bool handleSim::checkReg()
 {
